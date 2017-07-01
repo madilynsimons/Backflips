1e88b2cb3dca0dd25e71330da04ebc9ee7c7aa65
madilynsimons <madilynsimons@gmail.com>
2017-06-30 20:10:21 -0400

    Moved Attack and AttackCollection to their own .cs files

---
+++ Backflips/Assets/Scripts/Attack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+	readonly string DEFAULTTITLE = "nameless";
+	readonly int DEFAULTPP = 20;
+	readonly int DEFAULTDAMAGE = 10;
+	string title;
+	int maxPP, currentPP;
+	int damage;
+
+	public Attack()
+	{
+		title = DEFAULTTITLE;
+		maxPP = DEFAULTPP;
+		currentPP = DEFAULTPP;
+		damage = DEFAULTDAMAGE;
+	}
+
+	public Attack(string title, int pp, int damage)
+	{
+		this.title = title;
+		maxPP = pp;
+		currentPP = pp;
+		this.damage = damage;
+	}
+
+	public static bool operator ==(Attack a, Attack b)
+	{
+		return a.title == b.title && a.maxPP == b.maxPP && a.damage == b.damage;
+	}
+
+	public static bool operator !=(Attack a, Attack b)
+	{
+		return !(a.title == b.title && a.maxPP == b.maxPP && a.damage == b.damage);
+	}
+}
---
+++ Backflips/Assets/Scripts/AttackCollection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackList : MonoBehaviour {
+
+	readonly int MAXNUMOFATTACKS = 4;
+	LinkedList<Attack> a;
+	int numOfAttacks;
+	Attack tackle;
+
+	public AttackList()
+	{
+		a = new LinkedList<Attack>();
+		numOfAttacks = 1;
+		tackle = new Attack();
+	}
+
+	public void addAttack(Attack att)
+	{
+		if(numOfAttacks != MAXNUMOFATTACKS)
+		{
+			numOfAttacks++;
+			a.AddFirst(att);
+		}
+	}
+
+	public void update()
+	{
+		for(int x = 0; x < numOfAttacks; x++)
+		{
+			// TODO: finish this
+			// TODO: make a LinkedList class cuz the unity one is S H I T
+		}
+	}
+}
